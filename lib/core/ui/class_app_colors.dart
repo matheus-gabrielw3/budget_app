6bad2df581497939672b56217f32968bb8441d4f
Matheus Gabriel <matheus@w3soft.com.br>
2023-10-18 13:31:16 -0300

    First  commit

---
+++ lib/core/ui/class_app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//Classe de cores que irá comportar por toda a aplicação
+class ClassAppColors {
+  static const Color vermelho = Colors.red;
+  static const Color azul = Colors.blue;
+  static const Color azulEscuro =  Color(0xFF09223B);
+  static const Color amarelo = Colors.yellow;
+  static const Color amber = Colors.amber;
+  static const Color preto = Colors.black;
+  static const Color branco = Colors.white;
+  static const Color marrom = Colors.brown;
+  static const Color verde = Colors.green;
+  static const Color laranja = Colors.orange;
+  static const Color laranjaEscuro = Colors.deepOrange;
+  static const Color roxo = Colors.purple;
+  static const Color roxoEscuro = Colors.deepPurple;
+  static const Color cinza = Colors.grey;
+  static const Color teal = Colors.teal;
+  static const Color rosa = Colors.pink;
+  static const Color indigo = Colors.indigo;
+}
