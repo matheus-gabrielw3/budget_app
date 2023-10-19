1bfaeb1a326b83929c6fae1cc6899f064cfb68be
MarcosAssis25 <erinaldoassis3@gmail.com>
2023-10-19 14:00:13 -0300

    criando estilo de texto

--- lib/core/ui/class_estilos_textos.dart
+++ lib/core/ui/class_estilos_textos.dart
@@ -1,4 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ClassEstilosTextos {
-  
+  static final azul48px400 = GoogleFonts.roboto(
+    color: const Color(0xff44C2FD),
+    fontSize: 48,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final primary16px400 = GoogleFonts.roboto(
+    color: const Color(0xff000000),
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final roxo1_16px400 = GoogleFonts.roboto(
+    color: const Color(0xff343090),
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+   static final roxo2_20px500 = GoogleFonts.roboto(
+    color: const Color(0xff44C2FD),
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+
 }
