4a07fedff00b6f903bfb7d72ed49c3c2bcf8e7bc
Matheus Gabriel <matheus@w3soft.com.br>
2023-10-27 13:27:43 -0300

    Finaliza cadastro

--- lib/cadastro/pages/cadastro_fim.dart
+++ lib/cadastro/pages/cadastro_fim.dart
@@ -1,27 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:raro_budget_app/core/ui/class_app_colors.dart';
+import 'package:raro_budget_app/core/ui/class_estilos_textos.dart';
 
 class CadastroFim extends StatefulWidget {
   final PageController controller;
-  const CadastroFim({ super.key, required this.controller });
+  const CadastroFim({super.key, required this.controller});
 
   @override
   State<CadastroFim> createState() => _CadastroFimState();
 }
 
 class _CadastroFimState extends State<CadastroFim> {
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           body: SizedBox(
-            height: MediaQuery.of(context).size.height * 1,
-            width: MediaQuery.of(context).size.width * 1,
-            child: Column(
-              children: [
-                Image.asset('assets/images/backgroundfundo.png', fit: BoxFit.cover,)
-              ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/fundo.png"),
+                fit: BoxFit.cover,
+              ),
             ),
-           ),
-       );
+            child: null /* add child content here */,
+          ),
+          Container(
+            decoration:
+                BoxDecoration(color: Colors.deepPurple.withOpacity(0.7)),
+            child: null /* add child content here */,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+          const SizedBox(
+            height: 64,
+          ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/Vectorultime.png'),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 145.0),
+                child: Text(
+                  'Agora sim!\nVocê terá o\ncontrole\nfinanceiro nas\nsuas mãos!',
+                  style: ClassEstilosTextos.azul34w700roboto,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 235.0, top: 24),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: ClassAppColors.ciano,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(34))),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 12, bottom: 12, right: 6, left: 6),
+                    child:  Text('VAMOS LÁ!', style: ClassEstilosTextos.branco14w500roboto,),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
